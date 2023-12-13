d8867a4f063537380f19597fdca083d1c8637052
Niat0c <151126459+Niat0c@users.noreply.github.com>
2023-12-13 20:24:32 +0100

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppLogin } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppLogin', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppLogin],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppLogin);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have the 'Flowww' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppLogin);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Flowww');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppLogin);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Flowww');
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppLogin } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+//Carga la info
+bootstrapApplication(AppLogin, appConfig)
   .catch((err) => console.error(err));
